4153c90f4172a2573b3d958d09c82e110e290900
ae860 <44351081+ae860@users.noreply.github.com>
2018-11-13 19:24:20 -0700

    inital commet

---
+++ Print.py
@@ -0,0 +1,46 @@
+# # # Demonstration of the print statement 
+# # # Other information 
+# # print ("Hellow World")
+# # print ("Hellow World")
+
+# # print ("I can't do it")
+# # print ('scott sure "tries"')
+
+# # # Escape character 
+# # print ('this is the first line \n This is the second line')
+# print("\n ")
+
+# # printed integer and an integer string 
+# print(35)
+# print("35")
+
+# # Concatenation combining strings
+# firstName = "Aaron" # this is an inline comment
+# lastName  = "Egli"  # This stores the users last name 
+
+# print(firstName + " " + lastName) # firstName lastName 
+
+#     # other options
+# print (firstName,lastName) 
+
+# print ('(0) first name (1) last name'). format(firstName, lastName)
+
+# print integers 
+# firstNumber = 5 
+# secondNumber = 10
+# print(firstNumber + secondNumber)
+# print(firstNumber, secondNumber)
+# print('(0) is greater than (1)'.format(fristNumber, secondNumber)
+
+# perfrom math function
+# floatingnumber 
+hightestscore = 0.9485
+lowtestscore = 0.5343
+print('The high score was ' + hightestscore + '\nThe low score was ' + str(lowtestscore))
+print('The high score was (0...2f)\nThe low score was (1..2f)'.format(hightestscore. lowtestscore)
+
+print('The print a list of things'
+        'Apple\n'
+        'Bannana\n'
+        'Orange\n')
+
